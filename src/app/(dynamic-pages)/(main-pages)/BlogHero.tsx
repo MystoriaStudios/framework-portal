d581e90d4af4b749685e1ca8f434a4f9fb30a86c
dashwastakenx <contact.pear.casa@gmail.com>
2023-11-11 16:19:58 +0000

    update to cards

--- src/app/(dynamic-pages)/(main-pages)/BlogHero.tsx
+++ src/app/(dynamic-pages)/(main-pages)/BlogHero.tsx
@@ -9,7 +9,7 @@ export const BlogHero = ({items}: { items: Table<'items'>[] }) => {
       {items.length ? (
         <div className={'grid lg:grid-cols-2 xl:grid-cols-3 grid-span-row gap-4'}>
           {items.map((item) => (
-            <article key={item.id} className="flex max-w-xl flex-col items-start justify-between">
+            <article key={item.id} className="flex max-w-xl flex-col items-start justify-between shadow rounded-lg p-4">
               <div className="flex items-center gap-x-4 text-xs">
                 <time dateTime={item.created_at} className="text-gray-500">
                   {item.created_at}
@@ -23,7 +23,7 @@ export const BlogHero = ({items}: { items: Table<'items'>[] }) => {
               <div className="group relative">
                 <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                   <Link
-                    href={`/blog/${item.name}`}>
+                    href={`/blog/${item.id}`}>
                     <span className="absolute inset-0"/>
                     {item.name}
                   </Link>
