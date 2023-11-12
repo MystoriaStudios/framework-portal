9daefd5399b313e397ea0885ba68a55431b65a92
egirldevs <emily@Bunni.me>
2023-11-12 13:22:33 -0500

    f

--- src/app/(external-pages)/terms/page.tsx
+++ src/app/(external-pages)/terms/page.tsx
@@ -1,5 +1,12 @@
 import {H1} from "@/components/ui/Typography/H1";
 
 export default function Terms() {
-  return <H1>Terms</H1>;
+  return (
+    <>
+      <H1>Terms and Conditions</H1>
+      <h2>Last updated: Nov 11th, 2023</h2>
+      
+
+    </>
+  )
 }
